239c38427a1d73881a93ee8b0f573587298b0224
Vinícius Bruno <55645550+Viniburn@users.noreply.github.com>
2021-06-22 02:55:48 -0300

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -19,7 +19,7 @@ for i in list(carregar):
 link = input("\033[1;34mCopie o link do Vídeo aqui: ")
 yt = YouTube(link)
 
-ys = yt.streams.filter(res="720p").first()
+ys = yt.streams.filter(res="360p").first().download('/home/storage/downloads')
 
 def progress_bar(done):
     print("\033[1;97m\rBaixando ➤ [{0:50s}] {1:.1f}%".format('#' * int(done * 50), done * 100),end='')
@@ -28,8 +28,6 @@ def test():
     for n in range(101):
         progress_bar(n/100)
         time.sleep(0.01)
-        
-ys.download('/storage/downloads/')
 
 test()
 
