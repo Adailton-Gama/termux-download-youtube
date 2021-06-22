2a9e5d0e703ece2529dfaa4d891c19e6ce6dfab9
Vinícius Bruno <55645550+Viniburn@users.noreply.github.com>
2021-06-22 02:07:32 -0300

    youtube.py
    Este Programa foi desenvolvido para baixar vídeos do YouTube

---
+++ youtube.py
@@ -0,0 +1,34 @@
+from pytube import YouTube
+from tqdm import tqdm
+from time import sleep
+import pytube
+import sys
+import time
+import pyfiglet
+
+ascii_banner = pyfiglet.figlet_format("DownTube")
+print(ascii_banner)
+
+carregar = "\033[1;31m================> By Vini <================\n\n"
+
+for i in list(carregar):
+    print(i, end='')
+    sys.stdout.flush()
+    time.sleep(0.08)
+
+link = input("\033[1;34mCopie o link do Vídeo aqui: ")
+yt = YouTube(link)
+
+ys = yt.streams.filter(res="720p").first().download('/root/Downloads/')
+
+def progress_bar(done):
+    print("\033[1;97m\rBaixando ➤ [{0:50s}] {1:.1f}%".format('#' * int(done * 50), done * 100),end='')
+
+def test():
+    for n in range(101):
+        progress_bar(n/100)
+        time.sleep(0.01)
+
+test()
+
+print("\n\n\033[0;32mBaixado com Sucesso [ ✔️ ]")
