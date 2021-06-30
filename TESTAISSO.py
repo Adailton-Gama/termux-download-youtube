e0258d8900be5aab9f2c225d6d4b7219471dcaa1
Adailton-Gama <76784673+Adailton-Gama@users.noreply.github.com>
2021-06-30 17:42:20 -0300

    Create TESTAISSO.py

---
+++ TESTAISSO.py
@@ -0,0 +1,45 @@
+from pytube import YouTube
+from tqdm import tqdm
+from time import sleep
+import os
+import pytube
+import sys
+import time
+import pyfiglet
+
+os.system("clear")
+
+ascii_banner = pyfiglet.figlet_format("DownTube")
+print(ascii_banner)
+
+carregar = "\033[1;31m================> By Vini <================\n\n"
+
+for i in list(carregar):
+    print(i, end='')
+    sys.stdout.flush()
+    time.sleep(0.08)
+
+link = input("\033[1;34mCopie o link do Vídeo aqui: ")
+yt = YouTube(link)
+try:
+    yt.streams.filter(res="720p").first().download('download_video')
+except:
+    print('Não foi possível fazer download em FullHD, gostaria fazer download em uma resolução inferior? ')
+    qd = input('Pressione "1" para baixar ou "2" para cancelar ')
+    if qd == 1:
+        yt.streams.filter(res="360p").first().download('download_video')
+    elif qd==2:
+        print('Download Cancelado! Encerrando o programa')
+        quit()
+def progress_bar(done):
+    print("\033[1;97m\rBaixando ➤ [{0:50s}] {1:.1f}%".format('#' * int(done * 50), done * 100),end='')
+
+def test():
+    for n in range(101):
+        progress_bar(n/100)
+        time.sleep(0.01)
+test()
+
+os.system("mv download_video /storage/emulated/0/Download")
+
+print("\n\n\033[0;32mBaixado com Sucesso [ ✔️ ]")
