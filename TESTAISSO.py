c06de0681aac500811f3c73152497ae602ac7e9e
Adailton-Gama <76784673+Adailton-Gama@users.noreply.github.com>
2021-07-01 18:17:56 -0300

    Update TESTAISSO.py

--- TESTAISSO.py
+++ TESTAISSO.py
@@ -1,9 +1,6 @@
 from pytube import YouTube
-from tqdm import tqdm
-from time import sleep
-from sys import exit
 import os
-import pytube
+import shutil
 import sys
 import time
 import pyfiglet
@@ -22,26 +19,51 @@ for i in list(carregar):
 
 link = input("\033[1;34mCopie o link do Vídeo aqui: ")
 yt = YouTube(link)
-try:
-    yt.streams.filter(res="720p").first().download('download_video')
-except:
-    print('Não foi possível fazer download em FullHD, gostaria fazer download em uma resolução inferior? ')
-    qd = input('Pressione "1" para baixar ou "2" para cancelar ')
-    print()
-    if qd == 1:
-        yt.streams.filter(res="360p").first().download('download_video')
-    elif qd==2:
-        print('Download Cancelado! Encerrando o programa')
-def progress_bar(done):
+ntitle = yt.streams[0].title
+title = ntitle + ".mp4"
+print()
 
-    print("\033[1;97m\rBaixando ➤ [{0:50s}] {1:.1f}%".format('#' * int(done * 50), done * 100),end='')
+def progress_bar(done):
+    print("\033[1;97m\rBaixando ➤ [{0:50s}] {1:.1f}%".format('#' * int(done * 50), done * 100), end='')
+
 
 def test():
     for n in range(101):
-        progress_bar(n/100)
+        progress_bar(n / 100)
         time.sleep(0.01)
-test()
 
-os.system("move download_video c:/Users/Adailton/Downloads")
 
-print("\n\n\033[0;32mBaixado com Sucesso [ ✔️ ]")
+def move():
+    shutil.move("C:/Users/Adailton/PycharmProjects/PyDyout/download_video/{}".format(title), "c:/Users/Adailton/Downloads/{}".format(title))
+    print()
+    print('Alocando na Pasta Download, Por favor aguarde um pouco. ')
+
+
+def low_quality():
+    print('Não foi possível fazer download em FullHD, gostaria fazer download em uma resolução inferior? ')
+    qd = int(input('Pressione "1" para baixar ou "2" para cancelar: '))
+    print()
+    if qd == 1:
+        yt.streams.filter(res="360p").first().download('download_video')
+        test()
+        time.sleep(5)
+        move()
+        print("\n\n\033[0;32mBaixado com Sucesso [ ✔️ ]")
+    else:
+        print('Download Cancelado! Encerrando o programa')
+
+
+try:
+    yt.streams.filter(res="720p").first().download('download_video')
+    test()
+    print("\n\n\033[0;32mBaixado com Sucesso [ ✔️ ]")
+    time.sleep(5)
+    move()
+
+##  Parei aqui-----------------------------
+except:
+    low_quality()
+finally:
+    print('encerrendo o sistema')
+# print("ERRO AO BAIXAR")
+# print()
